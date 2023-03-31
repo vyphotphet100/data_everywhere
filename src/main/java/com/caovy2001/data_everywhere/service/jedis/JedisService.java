5041d8269628c982b5b356e1d0ab0d5091cd26b8
caovy2001 <caovy2001@gmail.com>
2023-03-31 12:53:17 +0700

    change type of connection of jedis

--- src/main/java/com/caovy2001/data_everywhere/service/jedis/JedisService.java
+++ src/main/java/com/caovy2001/data_everywhere/service/jedis/JedisService.java
@@ -6,14 +6,17 @@ import redis.clients.jedis.Jedis;
 
 @Component
 public class JedisService extends BaseService implements IJedisService {
-    private static final Jedis jedis = new Jedis("redis://default:DP52KjeuwAhZyjKrpvP3kBlOurft4mHi@redis-10810.c263.us-east-1-2.ec2.cloud.redislabs.com:10810");
+    private static final String jedisConnectionStr = "redis://default:DP52KjeuwAhZyjKrpvP3kBlOurft4mHi@redis-10810.c263.us-east-1-2.ec2.cloud.redislabs.com:10810";
+//    private static final Jedis jedis = new Jedis("redis://default:DP52KjeuwAhZyjKrpvP3kBlOurft4mHi@redis-10810.c263.us-east-1-2.ec2.cloud.redislabs.com:10810");
     @Override
     public String get(String key) {
+        Jedis jedis = new Jedis(jedisConnectionStr);
         return jedis.get(key);
     }
 
     @Override
     public void setWithExpired(String key, String value, long seconds) {
+        Jedis jedis = new Jedis(jedisConnectionStr);
         jedis.setex(key, seconds, value);
     }
 }
